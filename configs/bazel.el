0e7108966f38baaac88c8390d4a97f10b1992b65
Karol Czulkowski <karol.czulkowski@gmail.com>
2020-01-15 17:54:12 +0100

    Update bazel config.

--- configs/bazel.el
+++ configs/bazel.el
@@ -1,8 +1,8 @@
 ;; bazel-build mode
 
 (require 'url)
-(url-copy-file "https://raw.githubusercontent.com/bazelbuild/emacs-bazel-mode/master/bazel-build.el" "~/.emacs.d/configs/bazel-build.el" "ok-if-already-exists")
-(url-copy-file "https://raw.githubusercontent.com/bazelbuild/emacs-bazel-mode/master/bazel-mode.el" "~/.emacs.d/configs/bazel-mode.el" "ok-if-already-exists")
+(url-copy-file "https://raw.githubusercontent.com/bazelbuild/emacs-bazel-mode/master/lisp/bazel-build.el" "~/.emacs.d/configs/bazel-build.el" "ok-if-already-exists")
+(url-copy-file "https://raw.githubusercontent.com/bazelbuild/emacs-bazel-mode/master/lisp/bazel-mode.el" "~/.emacs.d/configs/bazel-mode.el" "ok-if-already-exists")
 (load "~/.emacs.d/configs/bazel-build")
 (load "~/.emacs.d/configs/bazel-mode")
 
