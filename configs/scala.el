518453dfdf004e3e1dee65fe4e0eaa3be48971c7
Karol Czulkowski <karol.czulkowski@gmail.com>
2020-08-24 15:15:03 +0200

    Update scala.el

--- configs/scala.el
+++ configs/scala.el
@@ -1,13 +1,17 @@
 (require 'use-package)
 
 ;; Enable defer and ensure by default for use-package
+;; Keep auto-save/backup files separate from source code:  https://github.com/scalameta/metals/issues/1027
 (setq use-package-always-defer t
-      use-package-always-ensure t)
+      use-package-always-ensure t
+      backup-directory-alist `((".*" . ,temporary-file-directory))
+      auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
 
-;; Enable scala-mode and sbt-mode
+;; Enable scala-mode for highlighting, indentation and motion commands
 (use-package scala-mode
   :mode "\\.s\\(cala\\|bt\\)$")
 
+;; Enable sbt mode for executing sbt commands
 (use-package sbt-mode
   :commands sbt-start sbt-command
   :config
@@ -16,31 +20,61 @@
   (substitute-key-definition
    'minibuffer-complete-word
    'self-insert-command
-   minibuffer-local-completion-map))
+   minibuffer-local-completion-map)
+   ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
+   (setq sbt:program-options '("-Dsbt.supershell=false"))
+)
 
+;; Enable nice rendering of diagnostics like compile errors.
 (use-package flycheck
   :init (global-flycheck-mode))
 
 (use-package lsp-mode
   ;; Optional - enable lsp-mode automatically in scala files
-  :hook (scala-mode . lsp)
+  :hook  (scala-mode . lsp)
+         (lsp-mode . lsp-lens-mode)
   :config (setq lsp-prefer-flymake nil))
 
-(setq lsp-file-watch-threshold nil)
+;; Add metals backend for lsp-mode
+(use-package lsp-metals)
 
-;; (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+;; Enable nice rendering of documentation on hover
 (use-package lsp-ui)
 
-;; C-M-. for searching symbol like class or sth.
-(use-package helm-lsp :commands helm-lsp-workspace-symbol)
-(global-set-key (kbd "C-M-.") 'helm-lsp-workspace-symbol)
+;; lsp-mode supports snippets, but in order for them to work you need to use yasnippet
+;; If you don't want to use snippets set lsp-enable-snippet to nil in your lsp-mode settings
+;;   to avoid odd behavior with snippets and indentation
+;;(use-package yasnippet)
 
 ;; Add company-lsp backend for metals
 (use-package company-lsp)
 
-;; lsp-mode -> what works:
-;; lsp-format-buffer -> starts scalafmt
-;; lsp-find-definition -> M-.
-;; lsp-find-references -> works and it would be great to connect this with helm
-(global-set-key (kbd "M-n") 'lsp-find-references)
+;; Use the Debug Adapter Protocol for running tests and debugging
+(use-package posframe
+  ;; Posframe is a pop-up tool that must be manually installed for dap-mode
+  )
+(use-package dap-mode
+  :hook
+  (lsp-mode . dap-mode)
+  (lsp-mode . dap-ui-mode)
+  )
+
 (global-set-key (kbd "C-c C-a") 'lsp-execute-code-action)
+(global-set-key (kbd "M-n") 'lsp-find-references)
+
+;; Use the Debug Adapter Protocol for running tests and debugging
+;;(use-package posframe
+  ;; Posframe is a pop-up tool that must be manually installed for dap-mode
+;;  )
+;;(use-package dap-mode
+;;  :hook
+;;  (lsp-mode . dap-mode)
+;;  (lsp-mode . dap-ui-mode)
+;;  )
+
+;; Use the Tree View Protocol for viewing the project structure and triggering compilation
+;;(use-package lsp-treemacs
+;;  :config
+;;  (lsp-metals-treeview-enable t)
+;;  (setq lsp-metals-treeview-show-when-views-received t)
+;;  )
