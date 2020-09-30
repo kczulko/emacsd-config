8ec8d802aad550d7d77f76410a33cdc953ae8eca
kczulko <karol.czulkowski@protonmail.com>
2020-09-30 13:33:17 +0200

    Remove not working package archive.

--- init.el
+++ init.el
@@ -3,7 +3,6 @@
 ;; bug workaround!
 ;; (setq package-check-signature nil)
 
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("melpa-org" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade.ferrier.me.uk/packages/"))
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
