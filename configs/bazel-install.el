1200bcb9230325cd7753fafcb7e2cd11d11157d9
kczulko <karol.czulkowski@protonmail.com>
2021-07-21 22:14:30 +0200

    Fix bazel installation.

--- configs/bazel-install.el
+++ configs/bazel-install.el
@@ -2,7 +2,7 @@
 
 (require 'url)
 (url-copy-file "https://raw.githubusercontent.com/bazelbuild/emacs-bazel-mode/master/bazel.el" "~/.emacs.d/configs/bazel.el" "ok-if-already-exists")
-(load "~/.emacs.d/configs/bazel-mode")
+(load "~/.emacs.d/configs/bazel")
 
 (require 'bazel)
 (add-to-list 'auto-mode-alist '("BUILD\\'" . bazel-mode))
